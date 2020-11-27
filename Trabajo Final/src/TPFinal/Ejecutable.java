448335ede5054a5646e6b56329fa94b2d7d035b4
abrilnoguera <63752328+abrilnoguera@users.noreply.github.com>
2020-11-27 14:12:46 -0300

    Update Ejecutable.java

--- Trabajo Final/src/TPFinal/Ejecutable.java
+++ Trabajo Final/src/TPFinal/Ejecutable.java
@@ -5,24 +5,25 @@ public class Ejecutable {
 
 	public static void main(String[] args) 
 	{
-		// Debe Crear un árbol binario de búsqueda, en el cual se almacene la información de las bandas que se han registrado
-		// en la ciudad de Buenos Aires, ordenado por el nombre del solista de cada banda (único).
+		// Debe Crear un árbol binario de búsqueda, en el cual se almacene la información de las bandas que se han registrado
+		// en la ciudad de Buenos Aires, ordenado por el nombre del solista de cada banda (único).
 		
 		Arbol arbol = new Arbol();
 		
-		// Debe Crear un Conjunto según los géneros de música que tocan las bandas.
+		// Debe Crear un Conjunto según los géneros de música que tocan las bandas.
 		Set<String> generos = new HashSet<>();
 		
-		// Debe Crear un Conjunto según los barrios en los que se han inscrito las bandas.
+		// Debe Crear un Conjunto según los barrios en los que se han inscrito las bandas.
 		Set<String> barrios = new HashSet<>();
 		
-		// Crear Map llamado MusicaPersonalidad, que relacione cada genero de música con la personalidad que define a sus amantes.
+		// Crear Map llamado MusicaPersonalidad, que relacione cada genero de música con la personalidad que define a sus amantes.
 		// Map<GENERO, PERSONALIDAD>
 		Map<String, String> MusicaPersonalidad = new HashMap<>();
 		
-		//Crear un Map llamado bandas que tenga como clave el género de música tocado por las bandas y como valor
+		//Crear un Map llamado bandas que tenga como clave el género de música tocado por las bandas y como valor
 		// el nombre del solista de cada banda, barrio y la cantidad de integrantes.
 
+		//hola
 	}
 
 }
